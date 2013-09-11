7c857fa92e78886041a74b165db4c04d35954676
Renne Rocha <rennerocha@gmail.com>
2013-09-11 15:54:18 -0300

    Function that returns the URL of all days in the year in 'BBC On This Day' site.

--- days.py
+++ days.py
@@ -1,26 +1,33 @@
 # -*- coding: utf-8 -*-
 
+MONTHS = {
+    'January': 31,
+    'February': 29,
+    'March': 31,
+    'April': 30,
+    'May': 31,
+    'June': 30,
+    'July': 31,
+    'August': 31,
+    'September': 30,
+    'October': 31,
+    'November': 30,
+    'December': 31,
+}
+
 def get_wikipedia_urls_for_whole_year():
     urls = []
-    
-    MONTHS = {
-        'January': 31,
-        'February': 29,
-        'March': 31,
-        'April': 30,
-        'May': 31,
-        'June': 30,
-        'July': 31,
-        'August': 31,
-        'September': 30,
-        'October': 31,
-        'November': 30,
-        'December': 31,
-    }
-
     for month, days in MONTHS.items():
         for day in xrange(1, days + 1):
             url = 'https://en.wikipedia.org/wiki/{0}_{1}'.format(month, day)
             urls.append(url)
+    return urls
 
+
+def get_bbc_on_this_day_urls_for_whole_year():
+    urls = []
+    for month, days in MONTHS.items():
+        for day in xrange(1, days + 1):
+            url = 'http://news.bbc.co.uk/onthisday/low/dates/stories/{0}/{1}/default.stm'.format(month.lower(), day)
+            urls.append(url)
     return urls
